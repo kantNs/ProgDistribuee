96d27ee7d25a8d3a5893637a8d8c2d28c7cf7275
kantNS <thebosssdu69@gmail.com>
2019-03-12 15:39:36 +0100

    2 threads but no semaphore usage for now

--- ProgDistribuee/src/Main.java
+++ ProgDistribuee/src/Main.java
@@ -1,6 +1,77 @@
+class decremente implements Runnable {
+    int i;
+    decremente(int i) {
+        this.i = i;
+    }
+    public void run() {
+        while(1==1) {
+            int j = i;
+            try {
+                Thread.sleep(1000);
+                i--;
+                // Displaying the thread that is running
+                System.out.println(" Decremente " +
+                        Thread.currentThread().getId() +
+                        " is running \n");
+                System.out.println("i a pour valeur " +
+                        i +
+                        " !!");
+            } catch (Exception e) {
+                // Throwing an exception
+                System.out.println("Exception is caught");
+            }
+            i = j;
+        }
+
+    }
+
+}
+
+class incremente implements Runnable {
+    int i;
+    incremente(int i) {
+        this.i = i;
+    }
+    public void run() {
+        while(1==1) {
+            int j = i;
+            try {
+                Thread.sleep(1000);
+                i++;
+                // Displaying the thread that is running
+                System.out.println(" Incremente  " +
+                        Thread.currentThread().getId() +
+                        " is running \n");
+                System.out.println("i a pour valeur " +
+                       i +
+                        " !!");
+            } catch (Exception e) {
+                // Throwing an exception
+                System.out.println("Exception is caught");
+            }
+            i = j;
+        }
+
+    }
+}
+
+
+
+
+
+
 public class Main {
 
+
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+// TODO code application logic here
+        int i=65;
+
+        Thread thread1 = new Thread(new decremente(i), "thread1");
+        Thread thread2 = new Thread(new incremente(i), "thread2");
+        thread1.start();
+        thread2.start();
+
     }
 }
